43af4ce6a2c8b2ab3378a6592b059436df6a2599
Aron Harsfalvi <aron.harsfalvi@gmail.com>
2024-06-09 19:38:05 +0200

    adds cors

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from rest.newsFetcherREST import news_fetcher_blueprint
 from rest.priceInfoREST import price_info_blueprint
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(news_fetcher_blueprint, url_prefix='/api')
 app.register_blueprint(price_info_blueprint, url_prefix='/api')
